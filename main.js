e6824a8669b84e7286d52bcd9455063668558eff
Paul Diamond <pauldiamond@enzyme.com>
2019-10-17 15:38:55 -0700

    TEST PATH 2

--- main.js
+++ main.js
@@ -15,6 +15,14 @@ async function run() {
     console.log('************', gitHubSecret, 'SECRET');
     console.log('************', myToken, 'TOKEN');
     console.log('************',context, 'CONTEXT')
+
+    console.log({owner: context.repository.full_name.split('/')[0],
+    repo: context.repository.full_name.split('/')[1],
+    pull_number: context.number,
+    body: 'Testing comment',
+    commit_id: context.pull_request.base.sha,
+    path: 'test.txt',
+    position: 1,});
   
     const prComment = await octokit.pulls.createComment({
       owner: context.repository.full_name.split('/')[0],
@@ -24,7 +32,6 @@ async function run() {
       commit_id: context.pull_request.base.sha,
       path: 'main.js',
       position: 1,
-
 
     })
   } catch (error) {
