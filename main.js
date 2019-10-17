8d50b9dc9157f09d535425d97f2ceef0e452b87d
Paul Diamond <pauldiamond@enzyme.com>
2019-10-17 15:54:16 -0700

    TESTING POST

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ async function run() {
       repo: context.repository.full_name.split('/')[1],
       pull_number: context.number,
       body: 'Testing comment',
-      commit_id: context.pull_request.base.sha,
+      commit_id: context.pull_request.head.sha,
       path: 'main.js',
       position: 1,
 
