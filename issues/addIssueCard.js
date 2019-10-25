e23265695732fd374d6403c6a545055ccd13374d
Paul Diamond <pauldiamond@enzyme.com>
2019-10-25 11:05:05 -0700

    Added ID

--- issues/addIssueCard.js
+++ issues/addIssueCard.js
@@ -8,14 +8,11 @@ async function run() {
     const octokit = new github.GitHub(gitHubSecret);
   
     const context = github.context.payload;
-    console.log(context)
 
-    // const postChecklistAsComment = await octokit.projects.createCard({
-    //   owner: context.repository.full_name.split('/')[0],
-    //   repo: context.repository.full_name.split('/')[1],
-    //   issue_number: context.number,
-    //   body: 'Please verify the following about your PR: \n- [ ] Does the code have tests\n- [ ] Have you thought about how this scales to larger amounts of data\n- [ ] Have you thought about soft deletion - does anything here need audit logging or soft delete to preserve historical data\n- [ ] Does this PR include data migration and will this impact correctness of data for live customers\n - [ ] If this PR has migrations, have you run [schema annotation](https://github.com/ctran/annotate_models)\n',
-    // })
+    const createIssueCard = await octokit.projects.createCard({
+      column_id: '6843168',
+      content_id: context.issue.id
+    })
   } catch (error) {
     core.setFailed(error.message);
   }
